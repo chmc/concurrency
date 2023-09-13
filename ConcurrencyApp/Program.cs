aaeeefa494ac8fe6d5a2ff0c74eabfc38052ef81
Aleksi Sutela <asutela@gmail.com>
2023-09-13 21:35:11 +0300

    Thread sample

--- ConcurrencyApp/Program.cs
+++ ConcurrencyApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main() 
+    {
+        Thread newThread = new Thread(DoWork);
+        Console.WriteLine($"Thread state: {newThread.ThreadState}");
+        newThread.Start();
+        Console.WriteLine($"Thread state: {newThread.ThreadState}");
+    }
+
+    static void DoWork()
+    {
+        Console.WriteLine("Hello from new thread!");
+    }
+}
