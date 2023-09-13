afa594cb42116c1bf15b77e12247cd844c268261
Aleksi Sutela <asutela@gmail.com>
2023-09-13 21:45:15 +0300

    Add tasks app

---
+++ 02_Tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main() 
+    {
+        Task newTask = Task.Run(() => DoWork());
+        newTask.Wait();
+    }
+
+    static void DoWork()
+    {
+        Console.WriteLine("Hello from new task!");
+    }
+}
