d3e7996fbf8b5c945311b2f1dd0215d3cf89168f
Aleksi Sutela <asutela@gmail.com>
2023-10-01 21:07:51 +0300

    Add continue

---
+++ 04_Continue/Program.cs
@@ -0,0 +1,9 @@
+﻿Task<int> initialTask = Task.Run(() => DoWork());
+Task continuationTask = initialTask.ContinueWith(prevTask => 
+    Console.WriteLine($"Result: {prevTask.Result}"));
+continuationTask.Wait();
+
+int DoWork()
+{
+    return 42;
+}
