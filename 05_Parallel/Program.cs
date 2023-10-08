9910e5519a340eb08eeb42ccd72762dfc4832efd
Aleksi Sutela <asutela@gmail.com>
2023-10-08 14:06:25 +0300

    Add 5 parallel basics

---
+++ 05_Parallel/Program.cs
@@ -0,0 +1,6 @@
+﻿// Parallel.For
+Parallel.For(0, 10, i => Console.WriteLine($"Hello from iteration {i} on thread {Thread.CurrentThread.ManagedThreadId}"));
+
+// Parallel.ForEach
+List<string> items = new List<string> { "apple", "banana", "cherry" };
+Parallel.ForEach(items, item => Console.WriteLine($"Processing {item} on thread {Thread.CurrentThread.ManagedThreadId}"));
