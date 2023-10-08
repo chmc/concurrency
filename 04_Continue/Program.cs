13a26a454e64b30a9ba658d94a656df389ad25f4
Aleksi Sutela <asutela@gmail.com>
2023-10-08 14:49:06 +0300

    Removed old projects

--- 04_Continue/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Task<int> initialTask = Task.Run(() => DoWork());
-Task continuationTask = initialTask.ContinueWith(prevTask => 
-    Console.WriteLine($"Result: {prevTask.Result}"));
-continuationTask.Wait();
-
-int DoWork()
-{
-    return 42;
-}
