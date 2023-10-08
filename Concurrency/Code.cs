c5041ca8e9e7f1fe113e884949c6dc6a136c6d1e
Aleksi Sutela <asutela@gmail.com>
2023-10-08 14:41:40 +0300

    Refactor to one project

---
+++ Concurrency/Code.cs
@@ -0,0 +1,92 @@
+namespace Concurrency;
+
+public static class Code_01_Threads
+{
+    public static void Run()
+    {
+        Thread newThread = new Thread(DoWork);
+        Console.WriteLine($"Thread state: {newThread.ThreadState}");
+        newThread.Start();
+        Console.WriteLine($"Thread state: {newThread.ThreadState}");
+    }
+
+    static void DoWork()
+    {
+        Console.WriteLine("Hello from new thread!");
+    }
+}
+
+public static class Code_02_Tasks
+{
+    public static void Run()
+    {
+        Task newTask = Task.Run(() => DoWork());
+        newTask.Wait();
+    }
+
+    static void DoWork()
+    {
+        Console.WriteLine("Hello from new task!");
+    }
+}
+
+public static class Code_03_CancelTask
+{
+    public static void Run()
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        CancellationToken token = cts.Token;
+
+        Task newTask = Task.Run(() => DoWork(token));
+        cts.CancelAfter(TimeSpan.FromSeconds(2));
+        try
+        {
+            newTask.Wait();
+        }
+        catch (AggregateException ex)
+        {
+            Console.WriteLine("Task was canceled. ", ex.ToString());
+        }
+
+    }
+
+    static void DoWork(CancellationToken token)
+    {
+        Console.WriteLine("Loop from 0 - 10");
+        for (int i = 0; i < 10; i++)
+        {
+            token.ThrowIfCancellationRequested();
+            Console.WriteLine($"{i + 1}. Hello from new task!");
+            Thread.Sleep(500);
+        }
+    }
+}
+
+public static class Code_04_Continue
+{
+    public static void Run()
+    {
+        Task<int> initialTask = Task.Run(() => DoWork());
+        Task continuationTask = initialTask.ContinueWith(prevTask =>
+            Console.WriteLine($"Result: {prevTask.Result}"));
+        continuationTask.Wait();
+    }
+
+    static int DoWork()
+    {
+        return 42;
+    }
+}
+
+public static class Code_05_Parallel
+{
+    public static void Run()
+    {
+        // Parallel.For
+        Parallel.For(0, 10, i => Console.WriteLine($"Hello from iteration {i} on thread {Thread.CurrentThread.ManagedThreadId}"));
+
+        // Parallel.ForEach
+        List<string> items = new List<string> { "apple", "banana", "cherry" };
+        Parallel.ForEach(items, item => Console.WriteLine($"Processing {item} on thread {Thread.CurrentThread.ManagedThreadId}"));
+    }
+}
---
+++ Concurrency/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Concurrency;
+
+if (args.Length == 1)
+{
+    switch (args[0])
+    {
+        case "1": Code_01_Threads.Run(); break;
+        case "2": Code_02_Tasks.Run(); break;
+        case "3": Code_03_CancelTask.Run(); break;
+        case "4": Code_04_Continue.Run(); break;
+        case "5": Code_05_Parallel.Run(); break;
+    }
+}
+else
+{
+    Console.WriteLine("Use argument to execute one of following options:");
+    Console.WriteLine("0 - Last option");
+    Console.WriteLine("1 - Threads");
+    Console.WriteLine("2 - Tasks");
+    Console.WriteLine("3 - Cancel Task");
+    Console.WriteLine("4 - Continue");
+    Console.WriteLine("5 - Parallel");
+}
